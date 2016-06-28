e2be838ecfc0fcc6c9ed0f371efaf421360fe8d2
Anna Rogers <2muddybarefeet@gmail.com>
2016-06-28 07:56:00 -0700

    add functioning color slider to my app

--- color_sliders/ViewController.swift
+++ color_sliders/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func colorChange (sender: UISlider) {
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if redSlider == nil {
+            return
+        }
+        
+        let r: CGFloat = CGFloat(redSlider.value)
+        let g: CGFloat = CGFloat(greenSlider.value)
+        let b: CGFloat = CGFloat(blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        
     }
-
 
 }
 
